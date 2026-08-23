b48207bf6405b439577a5936197dad0118842585
bit-bLt <t@drocipra.com>
2026-08-23 14:55:10 -0400

    Removed

--- _tp.sh
+++ _tp.sh
@@ -4,7 +4,7 @@
 WINE_URI="https://github.com/Kron4ek/Wine-Builds/releases/download/11.15/wine-11.15-amd64-wow64.tar.xz"
 DSOAL_URI="https://github.com/kcat/dsoal/releases/download/archive/DSOAL_r694.zip"
 #GAME_URI="https://github.com/Joshhhuaaa/EnhancedSCCTVersus/releases/download/v3.6/Enhanced-SCCT-Versus-v3.6.7z"
-GAME_URI="https://drive.usercontent.google.com/download?export=download&id=16U49MTU3PBzUNHp4FvlwJwLWQb8hyxZ4&confirm=t"
+GAME_URI=""
 DXVK_URI="https://github.com/doitsujin/dxvk/releases/download/v3.0.2/dxvk-3.0.2.tar.gz"
 
 HERE="$(cd "$(dirname "$0")" && pwd -P)"
